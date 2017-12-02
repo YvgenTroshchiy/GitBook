86b71d787e32367372bbfaf42016e01b7477a2b2
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-12-02 22:35:33 +0200

    Update ActivityScoped documentation

--- app/src/main/java/com/troshchiy/gitbook/di/ActivityScoped.java
+++ app/src/main/java/com/troshchiy/gitbook/di/ActivityScoped.java
@@ -7,10 +7,9 @@ import java.lang.annotation.RetentionPolicy;
 import javax.inject.Scope;
 
 /**
- * In Dagger, an unscoped component cannot depend on a scoped component. As
- * {@link AppComponent} is a scoped component ({@code @Singleton}, we create a custom
- * scope to be used by all fragment components. Additionally, a component with a specific scope
- * cannot have a sub component with the same scope.
+ In Dagger, an unscoped component cannot depend on a scoped component.
+ As{@link AppComponent} is a scoped component ({@code @Singleton}, we create a custom scope to be used by all fragment components.
+ Additionally, a component with a specific scope cannot have a sub component with the same scope.
  */
 @Documented
 @Scope
