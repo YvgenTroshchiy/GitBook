8d7b0664930a92d94965c3554b403364e1f1476c
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-12-02 22:41:57 +0200

    Remove "4" from ActivityScoped documentation

--- app/src/main/java/com/troshchiy/gitbook/di/ActivityBindingModule.kt
+++ app/src/main/java/com/troshchiy/gitbook/di/ActivityBindingModule.kt
@@ -9,7 +9,7 @@ We want Dagger.Android to create a Subcomponent which has a parent Component of 
 in our case that will be AppComponent. The beautiful part about this setup is that you never need to tell AppComponent that it is going to have all these subcomponents
 nor do you need to tell these subcomponents that AppComponent exists.
 We are also telling Dagger.Android that this generated SubComponent needs to include the specified modules and be aware of a scope annotation @ActivityScoped
-When Dagger.Android annotation processor runs it will create 4 subcomponents for us.
+When Dagger.Android annotation processor runs it will create subcomponents for us.
  */
 @Module
 abstract class ActivityBindingModule {
