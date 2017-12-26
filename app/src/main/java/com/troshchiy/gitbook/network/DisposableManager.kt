8d6c811e2b5414227c3335376d9797c2749e29bb
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-12-26 23:43:25 +0200

    Update DisposableManager comment

--- app/src/main/java/com/troshchiy/gitbook/network/DisposableManager.kt
+++ app/src/main/java/com/troshchiy/gitbook/network/DisposableManager.kt
@@ -3,7 +3,7 @@ package com.troshchiy.gitbook.network
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-// Am not sure about this class, because it behavior not obvious but anyway for a test...
+// I am not sure about this class, because it behavior not obvious but anyway...
 object DisposableManager {
 
     private val disposables = CompositeDisposable()
