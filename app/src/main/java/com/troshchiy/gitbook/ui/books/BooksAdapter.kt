a0b21f9c42418fc6f56402ddaa0b153c9ccac78e
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-12-28 22:09:03 +0200

    Update BooksAdapter

--- app/src/main/java/com/troshchiy/gitbook/ui/books/BooksAdapter.kt
+++ app/src/main/java/com/troshchiy/gitbook/ui/books/BooksAdapter.kt
@@ -1,23 +1,41 @@
 package com.troshchiy.gitbook.ui.books
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.troshchiy.gitbook.BR
+import com.troshchiy.gitbook.databinding.ListItemBookBinding
 import com.troshchiy.gitbook.network.models.Book
 import java.util.*
 
-class BooksAdapter(private val itemClick: (Book) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class BooksAdapter(private val itemClick: (Book) -> Unit) : RecyclerView.Adapter<BooksAdapter.BindingHolder>() {
 
-    private val books: List<Book> = Collections.emptyList()
+    val books = LinkedList<Book>()
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-//        holder.getBinding().setVariable(BR.item, books[position])
-//        holder.getBinding().executePendingBindings()
-    }
+    var data: List<Book> = LinkedList()
+        set(value) {
+            books.clear()
+            books.addAll(value)
+            notifyDataSetChanged()
+        }
 
     override fun getItemCount(): Int = books.size
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
+        val inflater = LayoutInflater.from(parent.context)
+//        val binding = DataBindingUtil.inflate<ListItemBookBinding>(inflater, R.layout.list_item_book, parent, false)
+        val binding: ListItemBookBinding = ListItemBookBinding.inflate(inflater, parent, false)
+        return BindingHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BindingHolder?, position: Int) {
+        holder?.bind(books[position])
+    }
+
+    inner class BindingHolder(private val binding: ListItemBookBinding?) : RecyclerView.ViewHolder(binding?.root) {
+        fun bind(book: Book) {
+            binding?.book = book
+//            binding?.setVariable(BR.book, book)
+            binding?.executePendingBindings()
+        }
     }
 }
--- app/src/main/java/com/troshchiy/gitbook/ui/books/MainActivity.kt
+++ app/src/main/java/com/troshchiy/gitbook/ui/books/MainActivity.kt
@@ -3,6 +3,7 @@ package com.troshchiy.gitbook.ui.books
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import com.troshchiy.gitbook.R
+import com.troshchiy.gitbook.extensions.logI
 import com.troshchiy.gitbook.network.models.Book
 import com.troshchiy.gitbook.ui.LoadDataActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +13,8 @@ import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 
 class MainActivity : LoadDataActivity() {
+
+    private var adapter: BooksAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +26,18 @@ class MainActivity : LoadDataActivity() {
     }
 
     private fun setupRecyclerView() {
-        val adapter = BooksAdapter {}
+        adapter = BooksAdapter { logI(loggerTag, "title: $title") }
 
         with(recyclerView) {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity)
-            this.adapter = adapter
+            adapter = this@MainActivity.adapter
         }
     }
 
     private fun handleBooks(books: List<Book>) {
         debug { "Books size: ${books.size}" }
+        adapter?.data = books
     }
 
     override fun showLoadingProgress() {
